fa109f75f2509c5b6352c342fbbaa27953d3a026
Abdullah1428 <abdullahcse1428@gmail.com>
2021-08-24 12:30:49 +0200

    finished all routes for auth

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,9 +1,20 @@
 import express from 'express'
+import jwt from 'jsonwebtoken'
 
 const router = express.Router()
 
 router.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there')
+  if (!req.session || !req.session.jwt) {
+    return res.send({ currentuser: null })
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!)
+
+    res.send({ currentuser: payload })
+  } catch (error) {
+    res.send({ currentuser: null })
+  }
 })
 
 export { router as currentUserRouter }
