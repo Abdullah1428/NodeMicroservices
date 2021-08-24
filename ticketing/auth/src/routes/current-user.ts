220fe9bcc4e8b424c2c23da41531c9997081a7d5
Abdullah1428 <abdullahcse1428@gmail.com>
2021-08-24 12:54:26 +0200

    finished auth service and added middlewares for current user and protected routes

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,20 +1,11 @@
 import express from 'express'
-import jwt from 'jsonwebtoken'
+
+import { currentUser } from '../middlewares/current-user'
 
 const router = express.Router()
 
-router.get('/api/users/currentuser', (req, res) => {
-  if (!req.session || !req.session.jwt) {
-    return res.send({ currentuser: null })
-  }
-
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!)
-
-    res.send({ currentuser: payload })
-  } catch (error) {
-    res.send({ currentuser: null })
-  }
+router.get('/api/users/currentuser', currentUser, (req, res) => {
+  res.send({ currentUser: req.currentUser || null })
 })
 
 export { router as currentUserRouter }
