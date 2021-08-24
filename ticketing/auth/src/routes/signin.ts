740c634934b0c1dbd4ec1cdfd8ceaa494d9a9f31
Abdullah1428 <abdullahcse1428@gmail.com>
2021-08-24 11:39:49 +0200

    added logic for signin route handler

--- ticketing/auth/src/routes/signin.ts
+++ ticketing/auth/src/routes/signin.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from 'express'
 import { body } from 'express-validator'
+import jwt from 'jsonwebtoken'
 
 import { validateRequest } from '../middlewares/validate-request'
 import { User } from '../models/user'
 import { BadRequestError } from '../errors/bad-request-error'
+
+import { Password } from '../services/password'
 
 const router = express.Router()
 
@@ -20,7 +23,38 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
-    res.send('Hi there')
+    const { email, password } = req.body
+
+    const existingUser = await User.findOne({ email })
+
+    if (!existingUser) {
+      throw new BadRequestError('Invalid credentials!')
+    }
+
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password
+    )
+
+    if (!passwordsMatch) {
+      throw new BadRequestError('Invalid credentials!')
+    }
+
+    // generate jwt
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email
+      },
+      process.env.JWT_KEY!
+    )
+
+    // store it on session object
+    req.session = {
+      jwt: userJwt
+    }
+
+    res.status(200).send(existingUser)
   }
 )
 
