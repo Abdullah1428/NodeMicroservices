5f3f9667ca8560fdc3c9effc14a4096a51f7d11d
Abdullah1428 <abdullahcse1428@gmail.com>
2021-08-28 13:09:25 +0200

    finished auth service

--- ticketing/auth/src/routes/__test__/current-user.test.ts
+++ ticketing/auth/src/routes/__test__/current-user.test.ts
@@ -1,16 +1,8 @@
 import request from 'supertest'
 import { app } from '../../app'
 
-it('response with details about current user', async () => {
-  const authResponse = await request(app)
-    .post('/api/users/signup')
-    .send({
-      email: 'test@test.com',
-      password: 'password'
-    })
-    .expect(201)
-
-  const cookie = authResponse.get('Set-Cookie')
+it('responds with details about current user', async () => {
+  const cookie = await global.getAuthCookie()
 
   const response = await request(app)
     .get('/api/users/currentuser')
@@ -19,4 +11,13 @@ it('response with details about current user', async () => {
     .expect(200)
 
   expect(response.body.currentUser.email).toEqual('test@test.com')
+})
+
+it('responds with null if not authenticated', async () => {
+  const response = await request(app)
+    .get('/api/users/currentuser')
+    .send()
+    .expect(200)
+
+  expect(response.body.currentUser).toEqual(null)
 })
--- ticketing/auth/src/test/setup.ts
+++ ticketing/auth/src/test/setup.ts
@@ -1,6 +1,11 @@
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import mongoose from 'mongoose'
 import { app } from '../app'
+import request from 'supertest'
+
+declare global {
+  var getAuthCookie: () => Promise<string[]>
+}
 
 let mongo: any
 
@@ -28,3 +33,20 @@ afterAll(async () => {
   await mongo.stop()
   await mongoose.connection.close()
 })
+
+global.getAuthCookie = async () => {
+  const email = 'test@test.com'
+  const password = 'password'
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email,
+      password
+    })
+    .expect(201)
+
+  const cookie = response.get('Set-Cookie')
+
+  return cookie
+}
